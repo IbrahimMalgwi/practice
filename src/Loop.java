95afa18c04f34ad5e27d3b03df209e889f2cbc2e
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-02-11 03:21:39 +0100

    loops

---
+++ src/Loop.java
@@ -0,0 +1,15 @@
+public class Loop {
+    public static void main(String[] args) {
+        int j = 10;
+        for (j = 0; j < 10; j++) {
+            System.out.println(j);
+        }
+
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
