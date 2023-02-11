bf39f270b647efdcde6c29eb28dc8a5cdbbd276c
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-02-11 15:06:32 +0100

    practiced with List

---
+++ src/List.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class List {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Mango");
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("peach");
+        fruits.add("melon");
+        fruits.add("watermelon");
+
+        int listSize = fruits.size();
+        String place = fruits.get(4);
+
+        fruits.set(2, "green banana");
+
+        fruits.remove(2);
+
+        fruits.clear();
+
+        System.out.println(fruits);
+        System.out.println(place);
+
+    }
+}
